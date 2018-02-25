97260b0a4a66956745cd902ece3c619d6c6d508d
Martin Ficzel <ficzel@sitegeist.de>
2018-02-25 16:34:45 +0100

    TASK: Add meaningful error message if no node was given.

--- Classes/Fusion/NodeMappingImplementation.php
+++ Classes/Fusion/NodeMappingImplementation.php
@@ -2,8 +2,8 @@
 namespace PackageFactory\AtomicFusion\Mapping\Fusion;
 
 use Neos\Flow\Annotations as Flow;
-use Neos\Fusion\FusionObjects\AbstractFusionObject;
-use Neos\Utility\ObjectAccess;
+use Neos\Fusion\Exception as FusionException;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Fusion\FusionObjects\RawArrayImplementation;
 use PackageFactory\AtomicFusion\Mapping\Domain\Model\NodeMapping;
 
@@ -27,15 +27,21 @@ class NodeMappingImplementation extends RawArrayImplementation
     }
 
     /**
+     * Render data as RawArray but store a wrapper object with additional
+     * node-reference for later automatically applying contentELementWrapping
+     * during rendering of `Neos.Fusion:Collection`
      *
-     *
+     * @throws \Neos\Fusion\Exception
      * @return array
      */
     public function evaluate()
     {
         $node = $this->getNode();
+        if (!($node && $node instanceof NodeInterface)) {
+            throw new FusionException(sprintf('The node-attribute is required and must be of type "NodeInterface" "%s" given.', is_object($node) ? get_class($node) : gettype($node)));
+        }
         $data = parent::evaluate();
-        $nodeMapping =  new NodeMapping($data, $node);
+        $nodeMapping = new NodeMapping($data, $node);
         return $nodeMapping;
     }
 }
